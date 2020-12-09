09d6d18efcda6b4c69da6f666aec93e97ac254d7
bkEvg <bk.evg@mail.ru>
2020-12-09 19:59:59 +0700

    postgres connected

--- bauer_dress/bauer_dress/settings.py
+++ bauer_dress/bauer_dress/settings.py
@@ -127,8 +127,12 @@ WSGI_APPLICATION = 'bauer_dress.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bauerdress',
+	    'USER': 'kirill',
+	    'PASSWORD': 'a18b10c01',
+	    'HOST': 'localhost',
+	    'PORT': '5432'
     }
 }
 
