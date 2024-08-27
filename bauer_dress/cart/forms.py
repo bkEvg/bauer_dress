ccc88672b3e987dfd11bd7d817fd1b3090e97b7d
Bauer Kirill <bk.evg@mail.ru>
2024-08-27 18:58:32 +0700

    compatibilities problems

--- bauer_dress/cart/forms.py
+++ bauer_dress/cart/forms.py
@@ -2,9 +2,9 @@ from django import forms
 from shop.models import Colors, Product, Color
 
 
-colors = [('{}'.format(color.name), '{}'.format(color.name)) for color in Color.objects.all()]
+# colors = [('{}'.format(color.name), '{}'.format(color.name)) for color in Color.objects.all()]
 
 class CartAddProductForm(forms.Form):
 	quantity = forms.IntegerField()
 	update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
-	color = forms.TypedChoiceField(choices=colors)
+	# color = forms.TypedChoiceField(choices=colors)
